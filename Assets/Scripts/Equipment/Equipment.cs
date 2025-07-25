5af3cbab66579821b0b5f0332fd66ac8e29a8554
YoonhoKim0527 <hjyang093@gmail.com>
2025-07-25 17:47:34 +0900

    equip mamuri

--- Assets/Scripts/Equipment/Equipment.cs
+++ Assets/Scripts/Equipment/Equipment.cs
@@ -4,9 +4,10 @@ namespace Vampire
     [System.Serializable]
     public class Equipment
     {
-        public string name;        // 장비 이름
-        public Sprite icon;        // 장비 아이콘
-        public int tier;           // 단계 (1, 2, 3...)
-        public EquipmentType type;  // ✅ 추가
+        public string name;
+        public Sprite icon;
+        public int tier;
+        public EquipmentType type;
+        public float multiply = 1.0f; // ✅ 기본값 1.0 (효과 없음)
     }
 }
